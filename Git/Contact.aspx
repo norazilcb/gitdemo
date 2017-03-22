73be236006c830e5105b7b785bed1bd96ee21cda
norazilcb <norazi@laksamanacollege.edu.bn>
2017-03-22 14:18:54 +0800

    Updating Contact.aspx

---
+++ Git/Contact.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="Git.Contact" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Contact Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Name:
+            <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Email:
+            <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Subject:&nbsp; <asp:TextBox ID="txtSubject" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Message:<br />
+            <asp:TextBox ID="txtMsg" runat="server" Height="106px" TextMode="MultiLine" Width="359px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnSendEmail" runat="server" OnClick="btnSendEmail_Click" Text="Send" />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Git/Contact.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Git
+{
+    public partial class Contact : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSendEmail_Click(object sender, EventArgs e)
+        {
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            System.Net.NetworkCredential credentials = 
+                new System.Net.NetworkCredential("norazi@gmail.com", "happyfam1ly");
+            smtpClient.Host = "smtp.gmail.com";
+            smtpClient.Port = 587;
+        
+            MailMessage msg = new MailMessage("norazi@gmail.com", txtEmail.Text);
+            msg.Subject = "Name: " + txtName.Text + " Subject: " + txtSubject.Text;
+            msg.Body = txtMsg.Text;
+            smtpClient.Send(msg);
+        }
+    }
+}
